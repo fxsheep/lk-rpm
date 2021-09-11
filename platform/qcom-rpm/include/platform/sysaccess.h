447755e58a0d5f8a08e911d7b14731d9e169e15f
Sheep Sun <sunxiaoyang2003@gmail.com>
2021-09-11 10:36:43 +0800

    Expose manual page select

--- platform/qcom-rpm/include/platform/sysaccess.h
+++ platform/qcom-rpm/include/platform/sysaccess.h
@@ -6,3 +6,4 @@ uint32_t sys_readl(uintptr_t addr);
 uint8_t sys_readb(uintptr_t addr);
 void sys_writel(uint32_t value, uintptr_t addr);
 void sys_writeb(uint8_t value, uintptr_t addr);
+void update_page_select(uint8_t page_select);
